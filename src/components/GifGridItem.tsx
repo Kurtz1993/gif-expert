e3ba40a3f92c02148eaaa0d2ffb4591567e5e4a5
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2021-03-30 22:37:44 -0600

    Add animate css

--- src/components/GifGridItem.tsx
+++ src/components/GifGridItem.tsx
@@ -2,7 +2,7 @@ import { GifInfo } from '../models/gif-info.model';
 
 export default function GifGridItem({ title, url }: GifInfo) {
   return (
-    <div className="card">
+    <div className="card animate__animated animate__fadeIn">
       <img className="card__img" src={url} alt={title} />
       <p className="card__title">{title}</p>
     </div>
